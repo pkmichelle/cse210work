b68ed959199adbe9508432f34b168d9f63c96d36
Paige Klingler <paigeklingler@Paiges-MacBook-Air.local>
2024-04-27 22:30:03 -0600

    Prep 2 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Examples from prep module:
+        
+        // Console.WriteLine("Hello Prep1 World!");
+        // int number = 5;
+        // number = number +3;
+        // Console.WriteLine(number);
+
+        // Console.Write("What is you fav color?");
+        // string color = Console.ReadLine();
+        // Console.WriteLine($"your color is{color}");
+
+        // int x, y;
+        // x = 5;
+        // y = 1;
+        // Console.WriteLine(x);
+        // // or 
+        // // int x = 5;
+        // // Console.WriteLine(x);
+
+        // Console.Write("What is your favorite color?");
+        // string color = Console.ReadLine();
+        
+        // if (x > y)
+        // {
+        //     Console.WriteLine("greater");
+        // }
+        // string school = "BYU-Idaho";
+        // Console.WriteLine($"I am studying at {school}");
+
+        Console.Write("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+        
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,86 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // if (x>y)
+        // {
+        //     Console.WriteLine("greater than");
+        // else if (x>z)
+        // {
+        //     Console.WriteLine("greater than z");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("less than both");
+        // }
+        // }
+        
+    // if (name == "John")
+    // {
+    //     Console.WriteLine("The name is John");
+    // }
+    // if (name != "Color")
+    // {
+    //     Console.WriteLine("That's not my fav");
+    // }
+    // if (name == "Peter" || name== "James" || name== "John")
+    // {
+    //     Console.WriteLine("this is a biblical name.");
+    // }
+    // if (name == "Brigham" && name== "Young")
+    // {
+    //     Console.WriteLine("Welcome bro Brigham");
+    // }
+    // if (!(name == "Peter" || name== "James" || name== "John"))
+    // {
+    //     Console.WriteLine("this is a biblical name.");
+    // }
+    // string valueInText = "42";
+    // int number = int.Parse(valueInText);
+
+    //
+
+        Console.Write("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int gradePercent = int.Parse(userInput);
+
+        string letter = "";
+
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradePercent < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade: {letter}");
+        
+        
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("You've passed. Great job!");
+        }
+        else
+        {
+            Console.WriteLine("You'll get it next time! You did not pass the class.");
+        }
+
     }
 }
