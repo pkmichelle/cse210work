b1488f0ddf387a00ce44f2e982fce260ba82e42f
pkmichelle <kli22008@byui.edu>
2024-06-20 17:49:29 -0600

    week 09 prepare assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1= new Square("blue", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("red", 6, 7);
+        shapes.Add(s2);
+        
+        Circle s3 = new Circle("green", 8);
+        shapes.Add(s3);
+        
+
+        foreach (Shape shape in shapes)
+        {
+            string _color = shape.GetColor();
+            double _area = shape.GetArea();
+
+            Console.WriteLine($"The {_color} shape has an area of {_area}");
+            Console.WriteLine();
+        }
     }
 }
